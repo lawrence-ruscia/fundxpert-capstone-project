992442c6cec8150e02c768261ec650b584e025aa
lawrence-ruscia <140231442+lawrence-ruscia@users.noreply.github.com>
2025-10-15 00:31:51 +0800

    fix(backend): handle cors error and database schema)

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -4,9 +4,23 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   DATABASE_URL: z.url(),
-}); 
+});
 
 const env = envSchema.parse(process.env);
 export const pool = new Pool({
   connectionString: env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // important for Supabase SSL
+  },
 });
+
+const init = async () => {
+  await pool.query('SET search_path TO public;');
+
+  const result = await pool.query(
+    'SELECT current_database(), current_schema();'
+  );
+  console.log('Connected to:', result.rows[0]);
+};
+
+init();
